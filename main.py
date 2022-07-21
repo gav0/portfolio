2a97e897a7f39becd5c9ff20ce7ce63c54ae0a45
Jason Gavriel <github@gavriel.com.au>
2022-07-21 18:38:54 +1000

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/robots.txt')
+@app.route('/sitemap.xml')
+def static_from_root():
+    return send_from_directory(app.static_folder, request.path[1:])
+
 @app.route("/")
 def index():
 	return render_template("index.html")
